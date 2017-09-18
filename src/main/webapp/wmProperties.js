0daf8f79c7d3efcd8cb03f0ba27c917512b3302c
KavyaSree <kavya.sree@wavemaker.com>
2017-09-18 10:26:19 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "zscuh",
+  "homePage" : "Main",
+  "name" : "zscuh",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
